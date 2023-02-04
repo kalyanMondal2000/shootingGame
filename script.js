bec05ecbddd75d11cc3fa604015268c5eac5eb4e
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2023-02-04 18:57:01 -0500

    class version, new functions

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-/*
-class selectPlayer{
-    constructor(images, area){
-        this.images = images; 
-        this.area = area; 
-    };
-    displayChoice(images, area){
-        images.forEach(image => {
-            area.appendChild(document.createElement("img").src = image)
-        });
+class menu{
+    constructor(charArray, choiceArea, gameArea, newImgs, width, height, backgroundcolor){
+        this.charArray = charArray;
+        this.choiceArea = choiceArea;
+        this.gameArea = gameArea;
+        this.newImgs = newImgs;
+        this.width = width
+        this.height = height
+        this.backgroundcolor = backgroundcolor
+    }
+    display(charArray, choiceArea){
+        this.charArray.forEach(element =>{
+            let image = document.createElement("img")
+            image.src = element;
+            image.id = "character";
+            this.newImgs.push(image)
+            this.choiceArea.appendChild(image);
+        })
+    }
+    drawGame(gameArea, width, height, backgroundcolor){
+        this.gameArea.style.width = this.width
+        this.gameArea.style.height = this.height
+        this.gameArea.style.backgroundColor= this.backgroundcolor
+    }
+    select(charArray, gameArea){
+        this.newImgs.forEach(element =>{
+            element.addEventListener("click", function(){
+                const source = element.src
+                const newImg = document.createElement("img")
+                newImg.src = source;
+                newImg.id = "character";
+                document.getElementById("gameArea").appendChild(newImg)
+            })
+        })
     }
 }
-*/
-
-charArray = ["char1.png", "char4.png", "char5.png", "char6.png"];
+charArray = ["char1.png", "char5.png", "char6.png"];
 choiceArea = document.getElementById("choiceArea");
- 
-charArray.forEach(element =>{
-    image = document.createElement("img")
-    image.src = element;
-    image.id = "character";
-    choiceArea.appendChild(image);
-})
+gameArea = document.getElementById("gameArea")
+let newImgs = [];
+
+menuDisp = new menu(charArray, choiceArea, gameArea, newImgs, "1000px", "500px", "cornflowerblue")
+disp= menuDisp.display();
+select = menuDisp.select();
+draw = menuDisp.drawGame();
